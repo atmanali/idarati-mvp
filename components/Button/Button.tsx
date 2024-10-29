e1b112223f292fb1a7c5c8c08fbd868bacf1e403
ALI <atman.ali@capgemini.com>
2024-10-29 15:16:39 +0100

    graphic charter and Button component are ready

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -6,14 +6,16 @@ import styles from "./Button.module.css";
 type Props = React.HTMLAttributes<HTMLButtonElement> & {
     variant?: 'plain' | 'outlined' | 'text';
     size?: 'small' | 'medium' | 'large';
-    color?: 'success' | 'error' | 'warning' | 'neutral'
+    color?: 'success' | 'error' | 'warning' | 'neutral' | 'info'
 }
 
-export default function Button({children, variant='text', color='neutral', size='small'}: Props) {
-    useEffect(()=>{
-        console.log(styles);
-    }, [])
-    return (<button className={`${styles.container} ${styles[color]} ${styles[variant]} ${styles[variant+color]} ${styles[size]}`}>
-        {children}
-    </button>)
+export default function Button({children, variant='text', color='neutral', size='small', ...props}: Props) {
+    return (
+        <button
+            {...props} 
+            className={`${styles.container} ${styles[color]} ${styles[variant]} ${styles[size]}`}
+        >
+            {children}
+        </button>
+    )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,38 +4,39 @@ export default function () {
     return (<>
         <h1>ici c'est un titre</h1>
         <h2>ici c'est un sous-titre</h2>
+        <h1>ici c'est un <Button variant='text' onClick={()=>console.log('clicked')}>bouton</Button> dans un titre</h1>
         <div>
-            <Button variant={'plain'} color='success' size='small'> plain </Button>
-            <Button variant={'outlined'} color='error' size='small'> outlined </Button>
-            <div>
-                normalement ici j'ai du texte
-                <Button variant={'text'} size='small'>bouton</Button>
-                et le bouton est au centre
-            </div>
+            <Button color='success'>success</Button>
+            <Button color='success' size='medium'>success</Button>
+            <Button color='success' size='large'>success</Button>
+
+            <Button color='success' variant='plain'>success</Button>
+            <Button color='success' variant='plain' size='medium'>success</Button>
+            <Button color='success' variant='plain' size='large'>success</Button>
+
+            <Button color='success' variant='outlined'>success</Button>
+            <Button color='success' variant='outlined' size='medium'>success</Button>
+            <Button color='success' variant='outlined' size='large'>success</Button>
         </div>
         <div>
-            <Button variant={'plain'} color='success' size='medium'> plain </Button>
-            <Button variant={'outlined'} color='error' size='medium'> outlined </Button>
-            <div>
-                normalement ici j'ai du texte
-                <Button variant={'text'} size='medium'>bouton</Button>
-                et le bouton est au centre
-            </div>
+            <Button color='error'>error</Button>
+            <Button color='error' variant='plain'>error</Button>
+            <Button color='error' variant='outlined'>error</Button>
         </div>
         <div>
-            <Button variant={'plain'} color='success' size='large'> plain </Button>
-            <Button variant={'outlined'} color='error' size='large'> outlined </Button>
-            <div>
-                normalement ici j'ai du texte
-                <Button variant={'text'} size='large'>bouton</Button>
-                et le bouton est au centre
-            </div>
+            <Button color='warning'>warning</Button>
+            <Button color='warning' variant='plain'>warning</Button>
+            <Button color='warning' variant='outlined'>warning</Button>
         </div>
-        <Button color='neutral'>et là c'est un bouton avec beaucoup de texte</Button>
-        <Button color='warning'>warning</Button>
-        <Button color='warning' variant='plain'>warning</Button>
-        <Button color='warning' variant='outlined'>warning</Button>
-        <span style={{color: "var(--foreground-color-primary)"}}>primary{' '}</span>
-        <span style={{color: "var(--foreground-color-secondary)"}}>secondary</span>
+        <div>
+            <Button color='neutral'>neutral</Button>
+            <Button color='neutral' variant='plain'>neutral</Button>
+            <Button color='neutral' variant='outlined'>neutral</Button>
+        </div>
+        <div>
+            <Button color='info'>info</Button>
+            <Button color='info' variant='plain'>info</Button>
+            <Button color='info' variant='outlined'>info</Button>
+        </div>
     </>)
 }
