18019ee1e48c90f086bb9ef2e4a18e6abb4f9400
ALI <atman.ali@capgemini.com>
2024-10-29 15:25:21 +0100

    template

--- components/TopBar/Topbar.tsx
+++ components/TopBar/Topbar.tsx
@@ -2,6 +2,10 @@ import React from "react";
 
 export default function TopBar() {
     return(<>
-        <header id="topbar">ok mec</header>
+        <header id="topbar">
+            <div>logo</div>
+            <div>links</div>
+            <div>user</div>
+        </header>
     </>)
 }
