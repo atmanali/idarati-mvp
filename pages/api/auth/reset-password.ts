db56482c8bc8674ba6c0f23893b8d2fbfe05dd80
atmanali <atman.adh.ali@gmail.com>
2024-11-21 16:30:00 +0100

    not hashed reset password

--- pages/api/auth/reset-password.ts
+++ pages/api/auth/reset-password.ts
@@ -1,11 +1,11 @@
-import { hashPassword, randomPassword } from "@utils/crypting";
+import { randomPassword } from "@utils/crypting";
 import { NextApiRequest, NextApiResponse } from "next";
 import { isSuccessfulDataFetching, query } from "prisma/client";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === 'GET' && req.query?.username){
         const username = req.query.username as string;
-        const password = hashPassword(randomPassword(0));
+        const password = randomPassword(0);
         const resetPassword = await query (
             prisma => prisma.users.update({
                 data: { password },
