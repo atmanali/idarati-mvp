affd00fa545ec6bcccb184d3743b4255a67688dd
atmanali <atman.adh.ali@gmail.com>
2024-12-11 14:11:48 +0100

    email is sending

--- pages/api/auth/reset-password.ts
+++ pages/api/auth/reset-password.ts
@@ -1,11 +1,15 @@
+import { sendEmail } from "@services/email";
 import { hashPassword, randomPassword } from "@utils/crypting";
 import { NextApiRequest, NextApiResponse } from "next";
 import { isSuccessfulDataFetching, query } from "prisma/client";
+
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === 'GET' && req.query?.username){
         const username = req.query.username as string;
         const password = randomPassword(0);
+        const isEmailSent = await sendEmail(username, password);
+        console.log(!isEmailSent || isEmailSent?.response?.status==200);
         const resetPassword = await query (
             prisma => prisma.users.update({
                 data: { password: hashPassword(password) },
@@ -13,8 +17,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             })
         )
         if (isSuccessfulDataFetching(resetPassword)) {
-            res.status(200).json({data: { password }})
+            res.status(200).json({data: {}})
         }
     }
-    res.status(500).json({data:{}})
+    else res.status(500).json({data:{}})
 }
