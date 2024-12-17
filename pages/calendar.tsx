3288d4834624bb8824a445e970c716171cb86fa0
atmanali <atman.adh.ali@gmail.com>
2024-12-17 15:06:50 +0100

    calendar displaying good dates

--- pages/calendar.tsx
+++ pages/calendar.tsx
@@ -1,11 +1,11 @@
 import styles from "@styles/pages/calendar.module.css";
 import { useAppointments } from "@services/appointments";
-import { arrayOfCalendarClassNames, classNames, dateToCalendarClassName, months } from "@utils/namings";
+import { arrayOfCalendarClassNames, months, monthsLengths } from "@utils/calendarUtils";
 import CalendarItem from "@components/CalendarItem/CalendarItem";
 import { useEffect, useState } from "react";
-import { AppointmentsModel } from "@models/index";
 import IconButton from "@components/IconButton/IconButton";
 import Label from "@components/Label/Label";
+import { classNames } from "@utils/namings";
 
 
 type TimeSlot = {
@@ -17,6 +17,7 @@ type Props = {
 }
 
 const lengthOfOneWeek = 1000*60*60*24*7;
+const lengthOfOneDay = 1000*60*60*24;
 
 export default function () {
     const { appointments, isFetched } = useAppointments();
@@ -24,35 +25,43 @@ export default function () {
     const [firstDayOfWeek, setFirstDayOfWeek] = useState<Date>();
     
     useEffect(() => {
-        isFetched && console.log(appointments);
+        isFetched && console.log({appointments, firstDayOfWeek});
     }, [isFetched])
 
     useEffect(() => {
-        setFirstDayOfWeek(today);
+        setFirstDayOfWeek(new Date(
+            today.getTime() - (today.getDay()-1)*lengthOfOneDay
+        ));
     }, [today])
 
-    const calculateFirstDayOfWeek = () => today?.getDate() - today?.getDay() + 1;
-    const firstDayOfWeekWithMonth = () => `${calculateFirstDayOfWeek()} ${months[today?.getMonth()]}`;
-    const lenghtOfActualMonth = () => months
+    const calculateDayOfWeek = (day: number = 0) =>{
+        const output = (firstDayOfWeek?.getDate()+day)%monthsLengths(firstDayOfWeek?.getFullYear())[firstDayOfWeek?.getMonth()];
+        return (!!day && output<6) ? output+1 : output;
+    }
+    
+    const firstDayOfWeekWithMonth = () => `${calculateDayOfWeek()} ${months[today?.getMonth()]}`;
 
     return (<>
         <div className={classNames([styles.calendarContainer, "vstack"])}>
             <header>
-                <div>
+                <div className="calendar-navigation-buttons">
                     <IconButton size="extraSmall" iconUrl="/chevron_left.svg" onClick={() => setToday(new Date(today.getTime() - lengthOfOneWeek))} />
                     <IconButton size="extraSmall" iconUrl="/chevron_right.svg" onClick={() => setToday(new Date(today.getTime() + lengthOfOneWeek))} />
                 </div>
-                <h4>Semaine du {firstDayOfWeekWithMonth()}</h4>
+                <div className="calendar-title">
+                    <span>Semaine du {firstDayOfWeekWithMonth()}</span>
+                    <span>{firstDayOfWeek?.getFullYear()}</span>
+                </div>
                 <Label className={styles.resetToday} onClick={() => setToday(new Date())}>Aujourd'hui</Label>
             </header>
             <div className={classNames([styles.calendarGrid])}>
-                <CalendarItem className={styles.mon}>Lundi {calculateFirstDayOfWeek()}</CalendarItem>
-                <CalendarItem className={styles.tue}>Mardi {calculateFirstDayOfWeek() + 1}</CalendarItem>
-                <CalendarItem className={styles.wed}>Mercredi {calculateFirstDayOfWeek() + 2}</CalendarItem>
-                <CalendarItem className={styles.thu}>Jeudi {calculateFirstDayOfWeek() + 3}</CalendarItem>
-                <CalendarItem className={styles.fri}>Vendredi {calculateFirstDayOfWeek() + 4}</CalendarItem>
-                <CalendarItem className={styles.sat}>Samedi {calculateFirstDayOfWeek() + 5}</CalendarItem>
-                <CalendarItem className={styles.sun}>Dimanche {calculateFirstDayOfWeek() + 6}</CalendarItem>
+                <CalendarItem className={styles.mon}>Lundi {calculateDayOfWeek()}</CalendarItem>
+                <CalendarItem className={styles.tue}>Mardi {calculateDayOfWeek(1)}</CalendarItem>
+                <CalendarItem className={styles.wed}>Mercredi {calculateDayOfWeek(2)}</CalendarItem>
+                <CalendarItem className={styles.thu}>Jeudi {calculateDayOfWeek(3)}</CalendarItem>
+                <CalendarItem className={styles.fri}>Vendredi {calculateDayOfWeek(4)}</CalendarItem>
+                <CalendarItem className={styles.sat}>Samedi {calculateDayOfWeek(5)}</CalendarItem>
+                <CalendarItem className={styles.sun}>Dimanche {calculateDayOfWeek(6)}</CalendarItem>
                 <CalendarItem className={styles.eight}>8h-9h</CalendarItem>
                 <CalendarItem className={styles.nine}>9h-10h</CalendarItem>
                 <CalendarItem className={styles.ten}>10h-11h</CalendarItem>
