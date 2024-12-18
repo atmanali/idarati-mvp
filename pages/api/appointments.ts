5e74d5330358e7070bbc1c1d345a3a84ba0bc808
atmanali <atman.adh.ali@gmail.com>
2024-12-18 22:20:27 +0100

    user asking for appointment is added to appointment by default

--- pages/api/appointments.ts
+++ pages/api/appointments.ts
@@ -2,6 +2,7 @@ import createAppointment from "@dataServices/appointments/createAppointments";
 import deleteAppointments from "@dataServices/appointments/deleteAppointments";
 import getAppointments from "@dataServices/appointments/getAppointments";
 import updateAppointments from "@dataServices/appointments/updateAppointments";
+import getUsers from "@dataServices/users/getUsers";
 import createUsersAppointments from "@dataServices/usersAppointments/createUsersAppointments";
 
 import { NextApiRequest, NextApiResponse } from "next";
@@ -16,14 +17,22 @@ export default async function (req: NextApiRequest, res: NextApiResponse){
             res.status(isSuccessfulDataFetching(gotAppointments) ? 200 : 500).send({ data: gotAppointments });
             break;
         case 'POST':
-            const {appointment, usersIds} = req.body;
+            let {appointment, usersIds} = req.body;
+            const [ username ] = req.headers.authorization.split(';');
+            usersIds = usersIds?.length
+                ? [
+                    ...usersIds,
+                    (await getUsers({ select: {id: true}, where: {username}}))[0]?.id
+                ] : [
+                    (await getUsers({select: {id: true}, where: {username}}))[0]?.id
+                ];
             const createdAppointment = await createAppointment(appointment);
             const relatedUsers = usersIds?.length ? (
                 await createUsersAppointments(
                     usersIds.map( userId => ({user_id: userId, appointment_id: createdAppointment?.id}) )
                 )
             ) : null;
-            res.status(isSuccessfulDataFetching(relatedUsers) ? 200 : 500).json({ data: createdAppointment });
+            res.status(isSuccessfulDataFetching(relatedUsers) || Boolean(createdAppointment) ? 200 : 500).json({ data: createdAppointment });
             break;
         case 'PATCH':
             const updatedAppointments = await updateAppointments(req.body);
